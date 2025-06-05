2d823c127223d9a3341d1bc40175970d6f938eec
nixbytes <real8686@gmail.com>
2025-06-04 22:55:31 -0400

    add initial project template

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+//Import Necessary Modules
+use std::io::{self,write};
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+/*
+ io is used for input/output.
+
+Command is used to run the clear command.
+
+sleep and Duration are used to pause the program like usleep.
+
+*/
+fn main() {
+    println!("Hello, world!");
+}
