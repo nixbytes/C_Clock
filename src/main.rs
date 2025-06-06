2f40ff481aca388682c8b1d3eb804c87a96950fc
nixbytes <real8686@gmail.com>
2025-06-05 22:13:43 -0400

    add This creates an iterator over these 3 parts Test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 //Import Necessary Modules
-use std::io::{self,write};
+use std::io::{self, Write};
 use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
 /*
- io is used for input/output.
+
+io is used for input/output.
 
 Command is used to run the clear command.
 
@@ -12,5 +13,18 @@ sleep and Duration are used to pause the program like usleep.
 
 */
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Please Set time (HH:MM:SS): ");
+
+    // Flush stdout to make sure the prompt is shown
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let parts: Vec<&str> = input.trim().split(':').collect();
+
+    println!("{}", parts[0]);
+    println!("{}", parts[1]);
+    println!("{}", parts[2]);
 }
